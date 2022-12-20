2d46b3fae6163f5991c5102029f6c98b79cfea68
icliniq-n7 <icliniq-n7@icliniqn7-pc>
2022-12-20 13:18:11 +0530

    List and set

---
+++ src/Listandset.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Listandset {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		List<String> list = new ArrayList<String>();
+//		list.add("apple");
+//		list.add("Grape");
+//		list.add("pine");
+//		list.add("apple");
+//		for (String value : list) {
+//			System.out.println(value);
+//		}
+////		boolean total = list.contains(list);
+//		String num = list.get(2);
+//		System.out.println(num);
+//		
+//		boolean val =list.contains("apple");
+//		System.out.println(val);
+//		
+//		list.remove(1);
+//		System.out.println(list);
+//		
+//		list.clear();
+//		System.out.println(list);
+		
+		Set<String> set = new TreeSet<String>();
+		set.add("nathi");
+		set.add("anand");
+		set.add("S");
+		set.add("nathi");
+		//for (String string : set) {
+			
+	//	}	
+	System.out.println(set);
+
+
+	}
+
+}
